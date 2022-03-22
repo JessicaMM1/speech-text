cf5fc73f984674de760ebb59172fd4cc366a6445
Jessica Martinez Marquez <jessmtzm@bu.edu>
2022-03-22 09:09:22 -0400

    threaded speech v1

--- speech-to-text.py
+++ speech-to-text.py
@@ -2,30 +2,57 @@
 
 import os
 from google.cloud import speech
+import threading, queue
 
-# Step 1. 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'health-app-2022-75164cf9a820.json'
-speech_client = speech.SpeechClient()
-audio_mp3 = 'never-give-up-and-good-luck-will-find-you.mp3'   # file is 6s long
+audio_list = ['never-give-up-and-good-luck-will-find-you.mp3' ]
 
-# Step 2. Create speech recongition instance (synchronous recognition)
-with open(audio_mp3, 'rb') as f1:
-	byte_data_mp3 = f1.read()
-audio_recongition = speech.RecognitionAudio(content=byte_data_mp3)
+q = queue.Queue()
 
-# Step 3. Configurations for audio file
-config_mp3 = speech.RecognitionConfig(
-    sample_rate_hertz=44000,
-    language_code="en-US",
-    enable_automatic_punctuation=True
-)
+def transcription():
+# Step 1. 
+    speech_client = speech.SpeechClient()
+    # audio_mp3 = 'never-give-up-and-good-luck-will-find-you.mp3'   # file is 6s long
+    
+    audio_mp3 = q.get()
 
-# Step 4. Transcribe audio file (MP3)
-response = speech_client.recognize(
-	config=config_mp3,
-	audio=audio_recongition
-)
+# def load_files():
+    # Step 2. Create speech recongition instance (synchronous recognition)
+    with open(audio_mp3, 'rb') as f1:
+        byte_data_mp3 = f1.read()
+    audio_recognition = speech.RecognitionAudio(content=byte_data_mp3)
 
 
-for result in response.results:
-    print(u"Transcript: {}".format(result.alternatives[0].transcript))
+# def configure():
+    # Step 3. Configurations for audio file
+    config_mp3 = speech.RecognitionConfig(
+        sample_rate_hertz=44000,
+        language_code="en-US",
+        enable_automatic_punctuation=True
+    )
+
+
+# def transcribe(config, audio_recognition):
+    # Step 4. Transcribe audio file (MP3)
+    response = speech_client.recognize(
+        config=config_mp3,
+        audio=audio_recognition
+    )
+
+    for result in response.results:
+        print(u"Transcript: {}".format(threading.current_thread().name, result.alternatives[0].transcript))
+
+
+def add_files():
+    for file in audio_list:
+        q.put(file)
+    
+        print(f'{threading.current_thread().name} adding {file}')
+
+
+threading.Thread(target=transcription, daemon=True).start()
+threading.Thread(target=add_files, daemon=True).start()
+
+
+q.join()
+print('All work completed')
